eba6e17c286a69abd97517f63308dd7a5a4acc50
KammampatiVaishnavi <144154884+KammampatiVaishnavi@users.noreply.github.com>
2023-10-02 21:59:31 +0530

    Add files via upload

---
+++ NoteCountDriver.java
@@ -0,0 +1,33 @@
+package com.gl.NoteCount;
+
+import java.util.Scanner;
+
+import com.gl.NoteCount.service.MergeSortImplementation;
+import com.gl.NoteCount.service.NotesCount;
+
+public class NoteCountDriver {
+
+	public static void main(String[] args) {
+		MergeSortImplementation mergeSortImplementation = new MergeSortImplementation();
+		NotesCount notesCount = new NotesCount();
+			
+		System.out.println("enter the size of currency denominations");
+		
+		Scanner sc= new Scanner(System.in);
+		
+		int size = sc.nextInt();
+		int[] notes = new int[size];
+		System.out.println("enter the  currency denominations  value");
+		for(int i=0;i<size;i++) {
+			notes[i] = sc.nextInt();
+			
+		}
+		System.out.println("enter the amount you want to pay");
+		int amount = sc.nextInt();
+		mergeSortImplementation.sort(notes,0,notes.length-1);
+		notesCount.notesCountImplementation(notes, amount);
+		sc.close();
+	}
+}
+
+
